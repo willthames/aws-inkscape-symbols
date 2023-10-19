b2a6cdcfdace64d79409e449194be7c8f0b1391c
Pim Snel <post@pimsnel.com>
2023-10-19 14:59:26 +0200

    fixes to make it work with latest Assests.zip as of 19okt 2023

--- build.sh
+++ build.sh
@@ -14,33 +14,39 @@ mkdir target build
 
 tempdir=`mktemp -d`
 
-bsdtar=`which bsdtar`
-if [ -f "$bsdtar" ]; then
-  bsdtar -xf $zipfile -s'|[^/]*/||' --include='*.svg' --exclude="._*" -C $tempdir
-  rsync -av $tempdir/ build --exclude GRAYSCALE/* --exclude __MACOSX
-else
-  unzip -q $zipfile "*.svg" -d $tempdir
-  rsync -av $tempdir/PNG*/ build --exclude GRAYSCALE/* --exclude __MACOSX
-fi
+unzip -q $zipfile "*.svg" -d $tempdir
+rsync -av $tempdir/ build --exclude GRAYSCALE/* --exclude __MACOSX
 
 rm -rf $tempdir
 
-servicedir=$(ls -d build/AWS-Architecture-Service*)
+servicedir=$(ls -d build/Architecture-Service*)
 if [ -d "$servicedir/Arch_Developer- Tools" ]; then
   mv "$servicedir/Arch_Developer- Tools" "$servicedir/Arch_Developer-Tools"
 fi
 
-for dir in build/AWS-Architecture-Resource*/*; do
+for dir in build/Resource*/*; do
   componentname=$(basename $dir | sed 's/^Res_//' | tr 'A-Z_' 'a-z-')
-  lightfiles=$dir/Res_48_Light/*.svg
+
+  lightfiles=$dir/*.svg
+  darkfiles=$dir/*.svg
+  if [ $componentname == "general-icons" ]; then
+    lightfiles=$dir/Res_48_Light/*.svg
+    darkfiles=$dir/Res_48_Dark/*.svg
+  fi
+
   python files_to_svg.py $componentname target/aws-$componentname-resource-light.svg $lightfiles
-  darkfiles=$dir/Res_48_Dark/*.svg
   python files_to_svg.py $componentname-dark target/aws-$componentname-resource-dark.svg $darkfiles
 done
 
-for dir in build/AWS-Architecture-Service*/*; do
+for dir in build/Architecture-Service*/*; do
   componentname=$(basename $dir | sed 's/^Arch_//' | tr 'A-Z_' 'a-z-')
   files=$dir/*48/*.svg
+  python files_to_svg.py $componentname target/aws-$componentname-service.svg $files
+done
+
+for dir in build/Category*/Arch-Category_48/**; do
+  componentname=$(basename $dir | sed 's/^Arch_//' | tr 'A-Z_' 'a-z-')
+  files=$(dirname $dir)/*.svg
   python files_to_svg.py $componentname target/aws-$componentname-service.svg $files
 done
 
